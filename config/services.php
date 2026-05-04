3a33676592c8b9aa1e14f4210ca6c1acd45fd229
malmental <orlandorojasoyarzun@gmail.com>
2026-05-04 16:02:06 +0200

    New AI provides phi3

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
 
     'ollama' => [
         'url' => env('OLLAMA_URL', 'http://localhost:11434'),
-        'model' => env('OLLAMA_MODEL', 'llama3.2:1b'),
+        'model' => env('OLLAMA_MODEL', 'phi3:latest'),
     ],
 
 ];
